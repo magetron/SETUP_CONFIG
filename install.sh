ba700fc97a1c2d4858c421baa9787813ee98b7f3
Patrick Wu <magetron1@gmail.com>
2020-09-11 06:55:25 -0700

    attempt to fix git theme

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ echo "setopt prompt_subst" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'bindkey "^[b" forward-word' >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'bindkey "^[f" backward-word' >> ${ZDOTDIR:-$HOME}/.zshrc
 
+echo "ZSH_THEME_GIT_PROMPT_DIRTY=' '" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "alias gst='git status'" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "alias size='f(){ du -sh $1* | sort -hr; }; f'" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'export GPG_TTY=$(tty)' >> ${ZDOTDIR:-$HOME}/.zshrc
