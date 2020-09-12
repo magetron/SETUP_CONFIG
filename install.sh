fccdb454643004ece1942ee82d9ff13f0fb03c7f
Patrick Wu <magetron1@gmail.com>
2020-09-12 21:41:33 +0100

    aware history zsh

--- install.sh
+++ install.sh
@@ -38,3 +38,8 @@ echo "ZSH_THEME_GIT_PROMPT_DIRTY=' '" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "alias gst='git status'" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "alias size='f(){ du -sh $1* | sort -hr; }; f'" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'export GPG_TTY=$(tty)' >> ${ZDOTDIR:-$HOME}/.zshrc
+
+echo "HISTFILE=~/.zsh_history" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "HISTSIZE=2147483647" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "SAVEHIST=2147483647" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "setopt appendhistory" >> ${ZDOTDIR:-$HOME}/.zshrc
